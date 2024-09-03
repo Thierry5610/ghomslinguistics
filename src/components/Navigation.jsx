407cd46b6bf286b6e17bcf08032bf595f9c65b69
Mctech6 <tiddingramsey@gmail.com>
2024-09-03 21:51:41 +0100

    Header almost done

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react';
 import Styles from './styles/navigation.module.scss'
 import { IoIosSearch,IoIosMenu} from "react-icons/io";
+import { IoCloseCircleOutline } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
 export default function Navigation() {
     const logoLink = "https://ghomslinguistics.com/wp-content/uploads/2024/08/cropped-GhomLinguisticsLogo_small.png"
     const [isMenu,setIsMenu] = useState(false)
-
+    const [isSearchMenu,setIsSearchMenu] = useState(false)
     return(
         <div className={Styles.wrapper}>
             <div className={Styles.container}>
                 <div className={Styles.content}>
+                    {isSearchMenu&&<div className={Styles.search_menu}>
+                        <input type="text" />
+                        <IoCloseCircleOutline onClick={()=>setIsSearchMenu(false)}/>
+                    </div>}
                     <div className={Styles.logo}>
                         <img src={logoLink} alt="logo" />
                     </div>
@@ -19,7 +24,7 @@ export default function Navigation() {
                         <div>Registration</div>
                         <div>Course</div>
                         <div>About us</div>
-                        <div className={Styles.search}><IoIosSearch /></div>
+                        <div className={Styles.search} onClick={()=>setIsSearchMenu(true)}><IoIosSearch /></div>
                     </div> 
                     {!isMenu&&<div className={Styles.menu} onClick={()=>setIsMenu(true)}><IoIosMenu/></div>}
                     {isMenu&&<div className={Styles.menu} onClick={()=>setIsMenu(false)}><IoMdClose/></div>}
