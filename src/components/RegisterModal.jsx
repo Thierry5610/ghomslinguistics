0a789431177759c2b9d5e658fe4944be69797625
Thierry <soyangthierry@gmail.com>
2024-10-09 20:22:51 +0100

    inputs in registermodal fixed

--- src/components/RegisterModal.jsx
+++ src/components/RegisterModal.jsx
@@ -18,8 +18,8 @@ const RegisterModal = ({ course, onClose }) => {
                 <h1>Fill in the form to register</h1>
                 <form className={Styles.contact_form}>
                     <input className={Styles.form_input} type="text" placeholder='Your name' />
-                    <input className={Styles.form_input} type="email" placeholder='Your surname' />
-                    <input className={Styles.form_input} type="email" placeholder='Telephone' />
+                    <input className={Styles.form_input} type="text" placeholder='Your surname' />
+                    <input className={Styles.form_input} type="tel" placeholder='Telephone' />
                     <input className={Styles.form_input} type="text" placeholder='Email' />
                     <textarea name="message" className={Styles.form_textarea} placeholder='Message' />
                     <button type="submit" className={Styles.cta}>Register</button>
