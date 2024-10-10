3f3b1445baf98c8eb300593a5a319a75b2ffd431
Thierry <soyangthierry@gmail.com>
2024-10-10 07:59:19 +0100

    new design for course register modal

--- src/pages/RegisterCourseModal.jsx
+++ src/pages/RegisterCourseModal.jsx
@@ -1,56 +1,68 @@
 import Styles from './styles/registercoursemodal.module.scss';
-import {MdPerson,MdCalendarMonth,MdLocationPin, MdBook, MdCalendarToday, MdPersonOutline, MdOutlineBook, MdOutlineLocationOn} from 'react-icons/md'
+import { MdPersonOutline, MdCalendarToday, MdOutlineBook, MdOutlineLocationOn, MdOutlineTimer3, MdOutlineWatch } from 'react-icons/md';
 import { IoMdClose } from "react-icons/io";
 import { useEffect } from 'react';
+import { LuCalendarDays, LuClock, LuMapPin, LuTag } from 'react-icons/lu';
 
-export default function RegisterCourseModal({language,onClose,onOpenCta}){
+export default function RegisterCourseModal({ language, onClose, onOpenCta }) {
     useEffect(() => {
         document.body.style.overflow = 'hidden';
         return () => {
             document.body.style.overflow = 'unset';
         };
     }, []);
-    return(
+
+    return (
         <div className={Styles.container}>
             <div className={Styles.content}>
-                <div className={Styles.close_button} onClick={onClose}>
+                <div className={Styles.closeButton} onClick={onClose}>
                     <IoMdClose />
                 </div>
-                <h1>Register for {language} courses </h1>
+                <h1>Register for {language} courses</h1>
                 <div className={Styles.cards}>
-                    <CourseCard level="A1" location="YDE" startDate="01/03" endDate="01/04" seats={4} price={"200000XAF"} action={onOpenCta}/>
-                    <CourseCard level="B1" location="DLA" startDate="01/03" endDate="01/04" seats={4} price={"120000XAF"} action={onOpenCta}/>
+                    <CourseCard level="A1" location="Yaounde" startDate="01/03" endDate="01/04" duration={4} price={"200000 XAF"} action={onOpenCta} />
+                    <CourseCard level="B1" location="Douala" startDate="01/03" endDate="01/04" duration={4} price={"120000 XAF"} action={onOpenCta} />
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-function CourseCard({level,startDate,endDate,location,seats,price,action}) {
-    return(
+function CourseCard({ level, startDate, endDate, location, duration, price, action }) {
+    return (
         <div className={Styles.card}>
-            <div className={Styles.details}>
-                <div className={Styles.level}>
-                    <MdOutlineBook/>
-                    <span>{level}</span>
+            <div className={Styles.cardHeader}>{level}</div>
+            <div className={Styles.cardContent}>
+                <div className={Styles.cardItem}>
+                    <LuMapPin/>
+                    <div className={Styles.cardItemText}>
+                        <span className={Styles.cardItemLabel}>Location</span>
+                        <span className={Styles.cardItemValue}>{location}</span>
+                    </div>
                 </div>
-                <div className={Styles.location}>
-                    <MdOutlineLocationOn/>
-                    <span>{location}</span>
+                <div className={Styles.cardItem}>
+                    <LuCalendarDays/>
+                    <div className={Styles.cardItemText}>
+                        <span className={Styles.cardItemLabel}>Date</span>
+                        <span className={Styles.cardItemValue}>{`${startDate} - ${endDate}`}</span>
+                    </div>
                 </div>
-                <div className={Styles.timeline}>
-                    <MdCalendarToday/>
-                    <span>{`${startDate} - ${endDate}`}</span>
+                <div className={Styles.cardItem}>
+                    <LuClock/>
+                    <div className={Styles.cardItemText}>
+                        <span className={Styles.cardItemLabel}>Duration</span>
+                        <span className={Styles.cardItemValue}>{duration} hours/day</span>
+                    </div>
                 </div>
-                <div className={Styles.seats}>
-                    <MdPersonOutline/>
-                    <span>{seats}</span>
+                <div className={Styles.cardItem}>
+                    <LuTag/>
+                    <div className={Styles.cardItemText}>
+                        <span className={Styles.cardItemLabel}>Price</span>
+                        <span className={Styles.cardItemValue}>{price}</span>
+                    </div>
                 </div>
-                <div className={Styles.price}>{price}</div>
             </div>
-            <button className={Styles.cta} onClick={action}>
-                Register
-            </button>
+            <button className={Styles.registerBtn} onClick={action}>Register Now</button>
         </div>
-    )
+    );
 }
