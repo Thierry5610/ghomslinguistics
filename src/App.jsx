a32bdadbd1c221379969adc18be13918a5a54247
Thierry <soyangthierry@gmail.com>
2024-10-17 13:27:37 +0100

    issue from adding browser router fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-// App.jsx
 import "./App.css";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
-import { Outlet } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import News from "./pages/News";
+import About from "./pages/About";
+import Course from "./pages/Course";
+import ScrollToTop from "./components/ScrollToTop"; 
+import Languages from "./pages/Languages";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
@@ -11,8 +16,14 @@ function App() {
       <Navigation />
       <ScrollToTop />
       <div className="content">
-        {/* Outlet serves as a placeholder for nested routes */}
-        <Outlet />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/language" element={<Languages />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/course/:language" element={<Course />} />
+        </Routes>
       </div>
       <Footer />
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,12 @@
-// Main.jsx or index.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.jsx';
-import Dashboard from './admin/Dashboard.jsx';
-import Home from './pages/Home';
-import News from './pages/News';
-import About from './pages/About';
-import Course from './pages/Course';
-import Languages from './pages/Languages';
-import Registration from './pages/Registration';
-import './i18n';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          {/* Nested routes under App */}
-          <Route index element={<Home />} />
-          <Route path="news" element={<News />} />
-          <Route path="about" element={<About />} />
-          <Route path="language" element={<Languages />} />
-          <Route path="register" element={<Registration />} />
-          <Route path="course/:language" element={<Course />} />
-        </Route>
-        <Route path="dashboard" element={<Dashboard />} />
-      </Routes>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
