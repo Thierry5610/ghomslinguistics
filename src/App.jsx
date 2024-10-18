7fd44512e9c028e0fae51f2ddf199c80dc9e30d2
Thierry <soyangthierry@gmail.com>
2024-10-18 01:21:23 +0100

    tried stuff with router and container class

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
+// App.jsx
 import "./App.css";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import News from "./pages/News";
-import About from "./pages/About";
-import Course from "./pages/Course";
-import ScrollToTop from "./components/ScrollToTop"; 
-import Languages from "./pages/Languages";
-import Registration from "./pages/Registration";
+import { Outlet } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <div className="container">
+    <div className="containerr">
       <Navigation />
       <ScrollToTop />
       <div className="content">
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/language" element={<Languages />} />
-          <Route path="/register" element={<Registration />} />
-          <Route path="/course/:language" element={<Course />} />
-        </Routes>
+        {/* Outlet serves as a placeholder for nested routes */}
+        <Outlet />
       </div>
       <Footer />
     </div>
