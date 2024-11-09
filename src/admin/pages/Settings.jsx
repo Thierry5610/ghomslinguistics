b94eceae7c883a48febad6d1922126e0ac0b0071
Thierry5610 <soyang.thierry4@gmail.com>
2024-11-09 16:24:50 +0100

    removed testing counter from settings

--- src/admin/pages/Settings.jsx
+++ src/admin/pages/Settings.jsx
@@ -4,7 +4,6 @@ export default function Settings () {
     return(
         <div className="flex text-stone-500 items-center justify-center">
             <div>Coming soon</div>
-            <div><NumberCounter number={5000}/></div>
         </div>
     )
 }
