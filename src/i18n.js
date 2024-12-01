51ad0d38911974e8b8808e96acaf7acf1a925b74
Thierry5610 <soyang.thierry4@gmail.com>
2024-12-01 01:07:10 +0100

    Locales created for admin pages

--- src/i18n.js
+++ src/i18n.js
@@ -15,6 +15,19 @@ import dataProtectionModalEN from './locales/en/dataProtectionModal.json'
 import navigationEN from './locales/en/navigation.json'
 import footerEN from './locales/en/footer.json'
 import courseEN from './locales/en/course.json'
+//admin translation files
+
+import adminCourseModalEN from './locales/en/admin-course-modal.json'
+import adminCourseEN from './locales/en/admin-courses.json'
+import adminDashboardEN from './locales/en/admin-dashboard.json'
+import adminHeaderEN from './locales/en/admin-header.json'
+import adminNewsModalEN from './locales/en/admin-news-modal.json'
+import adminNewsEN from './locales/en/admin-news.json'
+import adminSettingsEN from './locales/en/admin-settings.json'
+import adminStudentModalEN from './locales/en/admin-student-modal.json'
+import adminStudentsEN from './locales/en/admin-students.json'
+
+
 
 import homeFR from './locales/fr/home.json';
 import aboutFR from './locales/fr/about.json';
@@ -28,6 +41,19 @@ import dataProtectionModalFR from './locales/fr/dataProtectionModal.json'
 import navigationFR from './locales/fr/navigation.json'
 import footerFR from './locales/fr/footer.json'
 import courseFR from './locales/fr/course.json'
+//admin translation files
+
+import adminCourseModalFR from './locales/fr/admin-course-modal.json'
+import adminCourseFR from './locales/fr/admin-courses.json'
+import adminDashboardFR from './locales/fr/admin-dashboard.json'
+import adminHeaderFR from './locales/fr/admin-header.json'
+import adminNewsModalFR from './locales/fr/admin-news-modal.json'
+import adminNewsFR from './locales/fr/admin-news.json'
+import adminSettingsFR from './locales/fr/admin-settings.json'
+import adminStudentModalFR from './locales/fr/admin-student-modal.json'
+import adminStudentsFR from './locales/fr/admin-students.json'
+
+
 
 import homeDE from './locales/de/home.json';
 import aboutDE from './locales/de/about.json';
@@ -41,6 +67,17 @@ import dataProtectionModalDE from './locales/de/dataProtectionModal.json'
 import navigationDE from './locales/de/navigation.json'
 import footerDE from './locales/de/footer.json'
 import courseDE from './locales/de/course.json'
+//admin translation files
+
+import adminCourseModalDE from './locales/de/admin-course-modal.json'
+import adminCourseDE from './locales/de/admin-courses.json'
+import adminDashboardDE from './locales/de/admin-dashboard.json'
+import adminHeaderDE from './locales/de/admin-header.json'
+import adminNewsModalDE from './locales/de/admin-news-modal.json'
+import adminNewsDE from './locales/de/admin-news.json'
+import adminSettingsDE from './locales/de/admin-settings.json'
+import adminStudentModalDE from './locales/de/admin-student-modal.json'
+import adminStudentsDE from './locales/de/admin-students.json'
 
 // Configure i18next
 i18n
@@ -93,7 +130,7 @@ i18n
     },
     fallbackLng: 'en', // Fallback language
     supportedLngs: ['en', 'fr', 'de'], // Supported languages
-    ns: ['home', 'about', 'contactModal', 'languages','registration','news','registerCourseModal','registerModal','dataProtectionModal','navigation','footer','course'], // Namespaces for each page
+    ns: ['home', 'about', 'contactModal', 'languages','registration','news','registerCourseModal','registerModal','dataProtectionModal','navigation','footer','course','adminCourseModal', 'adminCourse', 'adminDashboard', 'adminHeader', 'adminNewsModal', 'adminNews', 'adminSettings', 'adminStudentModal', 'adminStudents'], // Namespaces for each page
     defaultNS: 'home', // Default namespace
     debug: true, // Turn off in production
     interpolation: {
